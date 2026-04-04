099f09a3cbe0ac2c84cfc4c954241f1f13b355e1
baymeric <aymeric.beaune@ynov.com>
2026-04-04 12:41:33 +0200

    [ :sparkles: ] - Account feature | -> PatchNote + Features terminé, reste à mettre à jour chaque jours.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,10 +9,18 @@ import { useRouter } from "next/navigation";
 export default function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
     const [showPaper, setShowPaper] = useState(false)
+    const [paperState, setPaperState] = useState(0)
 
-    const news = [
-        { title: "Déploiement", text: "..." },
-        { title: "Update", text: "..." }
+    const patchnote = [
+        { title: "05/04/2026", patch: [{ text: "Déconnexion fonctionnel" }, { text: "Création de compte terminé" }] },
+        { title: "04/04/2026", patch: [{ text: "Ajout de page Coming soon" }, { text: "Résolution de beug sur le fichier ressource" }, { text: "Ajout de la page d'accueil + nos challenges" }] },
+    ]
+
+    const features = [
+        { title: "Création des comptes" },
+        { title: "Login fonctionnel" },
+        { title: "Création du panel admin" },
+        { title: "Ajout de log" },
     ]
 
     useEffect(() => {
@@ -54,7 +62,7 @@ export default function Navbar() {
             {menuOpen && (
                 <div className="sm:hidden px-4 pb-4 animate-fadeIn">
                     <div className="flex flex-col gap-3">
-                        <button className="w-full text-left px-4 py-3 rounded-lg bg-[#2a2a3d] text-white/70 hover:bg-[#3a3a4d] transition">Nos challenges</button>
+                        <button className={` w-full text-left px-4 py-3 rounded-lg text-white/70 hover:bg-[#3a3a4d] transition`}>Nos challenges</button>
                         <button className="w-full text-left px-4 py-3 rounded-lg bg-[#2a2a3d] text-white/70 hover:bg-[#3a3a4d] transition">Mon compte</button>
                         <button className="w-full flex items-center gap-2 px-4 py-3 rounded-lg bg-red-500/20 text-red-400 hover:bg-red-500/30 transition"><MdExitToApp />Déconnexion</button>
                     </div>
@@ -64,17 +72,38 @@ export default function Navbar() {
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 backdrop-blur-sm">
                     <div className="w-full max-w-md bg-[#1e1e2f] border border-gray-700 rounded-2xl shadow-2xl p-6 animate-fadeIn">
                         <div className="flex justify-between items-center mb-4">
-                            <h2 className="text-xl font-bold text-white">PatchNotes</h2>
-                            <button onClick={() => setShowPaper(!showPaper)} className="text-gray-400 hover:text-white transition cursor-pointer">✕</button>
+                            <div className="flex items-center gap-5">
+                                <button onClick={() => setPaperState(0)} className={`${paperState == 0 ? "bg-[#2a2a3d] rounded-[5px] hover:bg-[#2a2a3d]/70 transition duration-500" : ""} text-xl font-bold text-white cursor-pointer hover:text-white/40 transition duration-500 p-2`}>PatchNotes</button>
+                                <button onClick={() => setPaperState(1)} className={`${paperState == 1 ? "bg-[#2a2a3d] rounded-[5px] hover:bg-[#2a2a3d]/70 transition duration-500" : ""} text-xl font-bold text-white cursor-pointer hover:text-white/40 transition duration-500 p-2`}>Features</button>
+                            </div>
+                            <button onClick={() => setShowPaper(!showPaper)} className="text-gray-400 hover:text-white transition cursor-pointer transition duration-500">✕</button>
                         </div>
                         <hr className="my-5 text-white" />
                         <div className="flex items-center flex-col gap-5">
-                            {news.map((el) => (
-                                <div key={el.title} className="flex flex-col justify-between items-center mb-4 w-full">
-                                    <h2 className="text-xl font-bold text-white">{el.title}</h2>
-                                    <p className="w-full mt-4 px-4 py-2 rounded-lg bg-[#2a2a3d] border border-gray-600 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500">{el.text}</p>
+                            {paperState == 0 && (
+                                <div className="w-full">
+                                    {patchnote.map((el) => (
+                                        <div key={el.title} className="flex flex-col justify-between items-center mb-4 w-full">
+                                            <h2 className="text-xl font-bold text-white">PatchNote du <span className="text-white/40 font-bold italic">{el.title}</span></h2>
+                                            <div className="w-full mt-4 px-4 py-2 rounded-lg bg-[#2a2a3d] border border-gray-600 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                                                {el.patch?.map((patch) => (
+                                                    <p>✔️ - {patch.text}</p>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
+                            )}
+                            {paperState == 1 && (
+                                <div className="flex flex-col justify-between items-center mb-4 w-full">
+                                    <h2 className="text-xl font-bold text-white">En cours de développement :</h2>
+                                    <div className="w-full mt-4 px-4 py-2 rounded-lg bg-[#2a2a3d] border border-gray-600 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                                        {features.map((el) => (
+                                            <p>⏳ {el.title}</p>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
